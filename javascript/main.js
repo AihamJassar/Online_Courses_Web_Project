934a345ab7b5baf41f06ac09db2431def50d5514
Aiham Jassar <aihamjassar@gmail.com>
2025-03-28 16:00:21 +0300

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,20 @@
-let links = document.querySelectorAll(".header .container .links li");
-console.log(links)
+let links = document.querySelectorAll(".header .container .links li a");
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    links.forEach(link => {
+      link.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
+let bars = document.getElementById("bars");
+bars.onclick = _ => {
+  let links = document.getElementById("links");
+  links.classList.toggle("open");
+};
+let userIcon = document.getElementById("user");
+userIcon.addEventListener("click", _ => {
+  let pupop = document.createElement("div");
+  pupop.classList.add("container", "pupop");
+  document.body.appendChild(pupop);
+});
